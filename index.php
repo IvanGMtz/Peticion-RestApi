ecd8f77db4eabee2285d491bef2bf0ed5d19e978
IvanGMtz <navi.garces24@gmail.com>
2023-06-09 16:00:11 -0500

    feat: :sparkles: Add method GET to show info users

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?php
+$url = 'https://6483457cf2e76ae1b95c3b21.mockapi.io/users';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Configuración de la API
-    $url = 'https://6483457cf2e76ae1b95c3b21.mockapi.io/users';
 
     // Crear un array con los datos a enviar a la API
     $data = array(
@@ -38,15 +36,51 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     // Verificar la respuesta de la API
     if ($response) {
-        echo "Datos enviados exitosamente a la API.";
     } else {
         echo "Error al enviar los datos a la API.";
     }
     }
 
     $_POST=array();
+    $data=array();
 }
 
+function createUser($url){
+    
+    $options = array(
+        'http' => array(
+            'header' => "Content-Type: application/json",
+            'method' => 'GET'
+        )
+    );
+    
+    $context = stream_context_create($options);
+    
+    // Realizar la solicitud a la API
+    $response = file_get_contents($url, false, $context);
+    
+    // Verificar la respuesta de la API
+    if ($response) {
+        // Decodificar la respuesta JSON en un array asociativo
+        $ApiDatos = json_decode($response, true);
+    
+        // Mostrar la información obtenida
+        foreach ($ApiDatos as $user) {
+            echo "<tr>
+            <td>" . $user["name"] . "</td>
+            <td>" . $user["surname"] . "</td>
+            <td>" . $user["addres"] . " </td>
+            <td>" . $user["age"] . " </td>
+            <td>" . $user["mail"] . " </td>
+            <td>" . $user["time"] . " </td>
+            <td>" . $user["team"] . " </td>
+            <td>" . $user["trainer"] . "</td>
+            </tr>";
+        }
+    } else {
+        echo "Error al obtener la información de la API.";
+    }
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -149,24 +183,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             <th>Hora/entrada</th>
                             <th>Team</th>
                             <th>Trainer</th>
-                            <th>Cedula</th>
                         </tr>
                 </thead>
                 <tbody id="table">
-                        <?php
-                        // echo "<tr>
-                        // <td>" . $_POST["nombre"] . "</td>
-                        // <td>" . $_POST["apellido"] . "</td>
-                        // <td>" . $_POST["direccion"] . " </td>
-                        // <td>" . $_POST["edad"] . " </td>
-                        // <td>" . $_POST["email"] . " </td>
-                        // <td>" . $_POST["hora"] . " </td>
-                        // <td>" . $_POST["team"] . " </td>
-                        // <td>" . $_POST["trainer"] . "</td>
-                        // <td>" . $_POST["cedula"] . " </td>
-                        // </tr>";
-                        // var_dump($_POST);
-                        ?>
+                        <?php createUser($url);  ?>
                 </tbody>
             </table>
         </footer>
